e29ed3a9aaa30a36e4ac5cbf929ce1a01e240ac7
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-23 04:49:50 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,60 @@
-
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+function App() {
+  const [taskInput, setTaskInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleTaskInputChange = (event) => {
+    setTaskInput(event.target.value);
+  };
+
+  const addTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, taskInput]);
+      setTaskInput('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (index) => {
+    const updatedTasks = [...tasks];
+    const newTask = prompt('Enter new task:', updatedTasks[index]);
+    if (newTask !== null) {
+      updatedTasks[index] = newTask.trim();
+      setTasks(updatedTasks);
+  };
 }
 
-export default App
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div className="add_tasks_section">
+        <input
+          type="text"
+          value={taskInput}
+          onChange={handleTaskInputChange}
+          placeholder="Enter a task"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <div className="tasks_section">
+        {tasks.map((task, index) => (
+          <div className="task" key={index}>
+            <span>{task}</span>
+            <button className="edit" onClick={() => editTask(index)}>Edit</button>
+            <button className="delete" onClick={() => deleteTask(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
